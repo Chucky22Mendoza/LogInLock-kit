f90318162544794b949cd5b8353231632d66ee38
Jesús Mendoza Verduzco <loginlock22@gmail.com>
2024-11-19 16:55:10 -0600

    Update docs of button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,18 +2,44 @@ import { AnchorHTMLAttributes, ButtonHTMLAttributes, DetailedHTMLProps } from 'r
 import styles from './button.module.css';
 import { cn } from '../../lib/utils';
 
-// Props de botón
+/**
+ * Type definition for ButtonProps, extending the default HTML button attributes.
+ *
+ * @property {string} [href] - Optional property, should be undefined for button elements.
+ */
 type ButtonProps =  DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
   href?: undefined;
 };
 
-// Anchor props
+/**
+ * Type definition for `AnchorProps`, which extends the default HTML anchor attributes.
+ *
+ * @property {string} [href] - An optional property representing the URL the anchor element points to.
+ *                             If not provided, the anchor will not navigate to any URL.
+ */
 type AnchorProps = DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> & {
   href?: string;
 };
 
+/**
+ * Renders a customizable button component that can function as either a button or an anchor element
+ * based on the presence of an href attribute. Supports various styles and icons.
+ *
+ * @param {ButtonAnchorProps} props - The properties for the button component, including size, variant,
+ * startIcon, endIcon, children, and className.
+ * @returns {JSX.Element} A button or anchor element styled according to the provided props.
+ */
 const hasHref = (props: ButtonProps | AnchorProps): props is AnchorProps => 'href' in props;
 
+/**
+ * Type definition for `ButtonAnchorProps`, which combines properties for both button and anchor elements.
+ *
+ * @property {'sm' | 'md' | 'lg'} [size] - Optional size of the button or anchor, determining its dimensions.
+ * @property {'primary' | 'outlined' | 'text'} [variant] - Optional variant style of the button or anchor.
+ * @property {React.ReactNode} [children] - Optional content to be displayed inside the button or anchor.
+ * @property {React.ReactElement} [startIcon] - Optional icon to be displayed at the start of the button or anchor.
+ * @property {React.ReactElement} [endIcon] - Optional icon to be displayed at the end of the button or anchor.
+ */
 export type ButtonAnchorProps = (ButtonProps | AnchorProps) & {
   size?: 'sm' | 'md' | 'lg';
   variant?: 'primary' | 'outlined' | 'text';
@@ -22,6 +48,16 @@ export type ButtonAnchorProps = (ButtonProps | AnchorProps) & {
   endIcon?: React.ReactElement;
 };
 
+/**
+ * Properties for the `Button` component, which can render as either a button or an anchor element.
+ *
+ * @property {'sm' | 'md' | 'lg'} [size='md'] - Optional size of the component, determining its dimensions.
+ * @property {'primary' | 'outlined' | 'text'} [variant='primary'] - Optional style variant of the component.
+ * @property {React.ReactNode} [children] - Optional content to be displayed inside the component.
+ * @property {React.ReactElement} [startIcon] - Optional icon to be displayed at the start of the component.
+ * @property {React.ReactElement} [endIcon] - Optional icon to be displayed at the end of the component.
+ * @property {string} [className] - Optional additional CSS class names to apply to the component.
+ */
 function Button(props: ButtonAnchorProps) {
   const {
     size = 'md',
